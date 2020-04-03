d6199f2d282ee1baeebbc1d221b77677b2db47c0
John Gallaugher <john.gallaugher@bc.edu>
2020-04-03 12:00:52 -0400

    First Commit

--- Button Chase/ViewController.swift
+++ Button Chase/ViewController.swift
@@ -7,14 +7,36 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var tapMeButton: UIButton!
+    
+    var audioPlayer: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    func playSound(name: String) {
+        if let sound = NSDataAsset(name: name) {
+            do {
+                audioPlayer = try AVAudioPlayer(data: sound.data)
+                audioPlayer.play()
+            } catch {
+                print("😡 ERROR: Could not play sound named: \(name)")
+            }
+        } else {
+            print("😡 ERROR: Couldn't load data from \(name)")
+        }
+    }
 
+    @IBAction func buttonTouched(_ sender: UIButton) {
+        let safeAreaFrame = self.view.safeAreaLayoutGuide.layoutFrame
+        let randomX = CGFloat.random(in: 0...(self.view.frame.width-tapMeButton.frame.width))
+        let randomY = CGFloat.random(in: safeAreaFrame.origin.y...(self.view.frame.height-tapMeButton.frame.height))
+        UIView.animate(withDuration: 0.3, animations: {self.tapMeButton.frame.origin = CGPoint(x: randomX, y: randomY)})
+        playSound(name: "chuckle")
+    }
 }
 
